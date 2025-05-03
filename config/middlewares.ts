713042e7967f4ee5cb64fd914e242e888ca1ed9c
jazilhashmi <jazilhashmi2017@gmail.com>
2025-05-03 23:50:25 +0500

    updated middleware.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,19 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'https://nextjs-pamphlet-site-vumu.vercel.app/', // ⬅️ Replace with your real domain
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: '*',
+      credentials: true,
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
